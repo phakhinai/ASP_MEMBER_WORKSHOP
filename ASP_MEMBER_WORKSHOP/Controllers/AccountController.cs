aef3750e124c3c17198f406153db8f3a4a0b7ceb
PHAKHINAI <PHAKHINAI@DESKTOP-2NDF5RJ>
2018-10-11 20:39:13 +0700

    End ASP2-2 - การเชื่อมต่อกับหน้าบ้าน Frontend

---
+++ ASP_MEMBER_WORKSHOP/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using ASP_MEMBER_WORKSHOP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP_MEMBER_WORKSHOP.Controllers
+{
+    public class AccountController : ApiController
+    {
+
+        // การลงทะเบียน
+        [Route("api/account/register")]
+        public IHttpActionResult PostRegister([FromBody] RegisterModel model)
+        {
+            if (ModelState.IsValid)
+                return Json(model);
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ ASP_MEMBER_WORKSHOP/Models/RegisterModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP_MEMBER_WORKSHOP.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string firstname { get; set; }
+
+        [Required]
+        public string lastname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+        [Required]
+        [Compare("password")]
+        public string cpassword { get; set; }
+    }
+}
